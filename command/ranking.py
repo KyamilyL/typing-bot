db1fc1e7a3a027db5d4f1e654db37168fc4e4da2
きゃみりー <112258920+KyamilyL@users.noreply.github.com>
2025-02-07 22:36:49 +0900

    add ranking command

--- command/ranking.py
+++ command/ranking.py
@@ -1,6 +1,70 @@
 import discord
+import datetime
 
 from discord import app_commands
+from management.data.bestscore import load_bestscores
+from management.user import running
+from config import client
 
-# @app_commands.command(name="ranking", description="ランキングを見れるよ！")
-# @app_commands.describe()
+@app_commands.command(name="ranking", description="ランキングを見れるよ！")
+@app_commands.describe(difficulty="難易度をえらべるよ！")
+@app_commands.describe(type="ランキングの種類をえらべるよ！")
+@app_commands.choices(
+    difficulty=[
+        app_commands.Choice(name="easy", value="easy"),
+        app_commands.Choice(name="normal", value="normal"),
+        app_commands.Choice(name="hard", value="hard")
+    ]
+)
+@app_commands.choices(
+    type=[
+        app_commands.Choice(name="server", value="server"),
+        app_commands.Choice(name="global", value="global")
+    ]
+)
+async def ranking(interaction: discord.Interaction, difficulty: str = None, type: str = None):
+
+    if interaction.user.id in running:
+        await interaction.response.send_message(
+            embed=discord.Embed(
+                description="実行中",
+                color=0xff6464
+            ),
+            ephemeral=True
+        )
+        return
+    
+    data = load_bestscores()
+    
+    if difficulty is None:
+        difficulty = "easy"
+
+    if type is None:
+        type = "server"
+
+    if type == "server":
+        guild = client.get_guild(interaction.guild.id)
+        members = {str(member.id) for member in guild.members}
+        users = sorted(
+            [userid for userid in data[difficulty] if userid in members],
+            key=lambda user: data[difficulty][user],
+            reverse=True
+        )
+        
+        await interaction.response.send_message(embed=discord.Embed(
+            title=f"ランキング モード:{difficulty}",
+            description="\n".join([f"**#{i+1}** <@{user}>: `{data[difficulty][user]}点`" for i, user in enumerate(users[:10])]),
+            colour=0x6464ff
+        ))
+    else:
+        users = sorted(
+            [userid for userid in data[difficulty]],
+            key=lambda user: data[difficulty][user],
+            reverse=True
+        )
+
+        await interaction.response.send_message(embed=discord.Embed(
+            title=f"グローバルランキング モード:{difficulty}",
+            description="\n".join([f"**#{i+1}** <@{user}>: `{data[difficulty][user]}点`" for i, user in enumerate(users[:10])]),
+            colour=0x6464ff
+        ))
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import config
 from discord import app_commands
 
 from command.practice import practice
-from management.database import setup_database
+from command.ranking import ranking
+from management.data.bestscore import setup_bestscore
 from management.word import create_image
 
 client = config.client
@@ -14,12 +15,13 @@ tree = app_commands.CommandTree(client)
 @client.event
 async def on_ready():
     print(f"{client.user.name} is online!")
-    setup_database()
+    setup_bestscore()
     create_image()
 
     await client.change_presence(activity=discord.Game(name=f"{len(client.guilds)} Servers!"))
     await tree.sync()
 
 tree.add_command(practice)
+tree.add_command(ranking)
 
 client.run(config.TOKEN)
