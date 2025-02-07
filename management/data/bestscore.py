ea8fa9e455b11a9c71eb027acd8c9243d58b38f1
きゃみりー <112258920+KyamilyL@users.noreply.github.com>
2025-02-07 22:37:44 +0900

    add bestcore

---
+++ management/data/bestscore.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+def setup_bestscore():
+    if not os.path.exists("data/bestscore.json"):
+        os.makedirs("data/bestscore.json")
+        with open("data/bestscore.json", "w") as file:
+            json.dump({"easy": {},"normal": {}, "hard": {}}, file)
+
+def load_bestscores():
+    with open("data/bestscore.json", "r") as file:
+        return json.load(file)
+
+def save_bestscores(data):
+    try:
+        with open("data/bestscore.json", "w") as file:
+            json.dump(data, file)
+    except FileNotFoundError:
+        return {}
+    except json.JSONDecodeError:
+        return {}
+    
+def get_bestscores(difficulty, userid):
+    data = load_bestscores()
+    if str(userid) not in data[difficulty]:
+        data[difficulty][str(userid)] = 0
+    save_bestscores(data)
+    return data[difficulty][str(userid)]
+
+def set_bestscore(difficulty, userid, score):
+    data = load_bestscores()
+    data[difficulty][str(userid)] = score
+    save_bestscores(data)
