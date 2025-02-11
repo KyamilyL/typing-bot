c4dacb4f3577655a34d6bf73e9b64b15ea75982f
きゃみりー <112258920+KyamilyL@users.noreply.github.com>
2025-02-12 02:23:22 +0900

    add stop command

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import config
 
 from discord import app_commands
 
-from command.practice import practice
 from command.ranking import ranking
+from command.stop import stop
+from command.typing import typing
+
 from management.word import create_image
 
 client = config.client
@@ -19,7 +21,8 @@ async def on_ready():
     await client.change_presence(activity=discord.Game(name=f"{len(client.guilds)} Servers!"))
     await tree.sync()
 
-tree.add_command(practice)
 tree.add_command(ranking)
+tree.add_command(stop)
+tree.add_command(typing)
 
 client.run(config.TOKEN)
